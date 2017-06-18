c709f9c5c46fab5a579af32fbb0f6af93cd3dff3
achernyak <artemchernyak@gmail.com>
2017-06-18 11:16:05 -0500

    better create member

--- src/clj/fcc_tracker/routes/services/members.clj
+++ src/clj/fcc_tracker/routes/services/members.clj
@@ -1,15 +1,18 @@
 (ns fcc-tracker.routes.services.members
   (:require [fcc-tracker.db.core :as db]
             [fcc-tracker.routes.services.utils :as utils]
-            [ring.util.http-response :refer :all]))
+            [fcc-tracker.validation :as v]
+            [ring.util.http-response :as response :refer :all]))
 
-(defn create-member! [org {:keys [fcc_username, name]}]
-  (try
-    (db/create-member! {:organization org
-                        :fcc_username fcc_username
-                        :name name})
-    (ok {:result :ok})
-    (catch Exception e
-      (utils/handle-duplicate-error
-       e
-       "member with the selected FreeCodeCamp username already exists"))))
+(defn create-member! [org member]
+  (if-let [error-message (v/member-creation-errors member)]
+    (response/precondition-failed {:result :error
+                                   :message error-message})
+    (try
+      (db/create-member!
+       (assoc member :organization org))
+      (ok {:result :ok})
+      (catch Exception e
+        (utils/handle-duplicate-error
+         e
+         "member with the selected FreeCodeCamp username already exists")))))
