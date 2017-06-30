d3006c02172e952a69b2045648c93be7b00d9e71
achernyak <artemchernyak@gmail.com>
2017-06-30 07:25:03 -0500

    database tests

--- test/clj/fcc_tracker/test/db/core.clj
+++ test/clj/fcc_tracker/test/db/core.clj
@@ -10,27 +10,46 @@
   :once
   (fn [f]
     (mount/start
-      #'fcc-tracker.config/env
-      #'fcc-tracker.db.core/*db*)
+     #'fcc-tracker.config/env
+     #'fcc-tracker.db.core/*db*)
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
 
 (deftest test-users
   (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-               t-conn
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
+    (is (= 1 (db/create-org!
+              t-conn
+              {:id         "1"
+               :pass       "pass"})))
     (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
             :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+            :org_name nil
+            :is_active nil
+            :pass       "pass"}
+           (db/get-org t-conn {:id "1"})))
+    (is (= 1 (db/delete-org!
+              t-conn
+              {:id "1"})))
+    (is (= nil (db/get-org t-conn {:id "1"})))))
+
+(deftest test-members
+  (jdbc/with-db-transaction [t-conn *db*]
+    (jdbc/db-set-rollback-only! t-conn)
+    (is (= 1 (db/create-member!
+              t-conn
+              {:organization "1"
+               :name "test"
+               :fcc_username "test"})))
+    (is (= [{:name "test"
+             :fcc_username "test"}]
+           (db/list-members
+            t-conn
+            {:organization "1"})))
+    (is (= 1 (db/delete-member!
+              t-conn
+              {:organization "1"
+               :fcc_username "test"})))
+    (is (= [] (db/list-members
+               t-conn
+               {:organization "1"})))))
